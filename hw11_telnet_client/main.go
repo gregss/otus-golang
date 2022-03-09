6985627d2326a24526b014fbea8555c6833728cd
Lyskov Igor <igor.lyskov@vseinstrumenti.ru>
2022-03-09 23:48:23 +0300

    hw11_telnet_client

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,37 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeoutf := flag.String("timeout", "1s", "timeout in second")
+	flag.Parse()
+	timeout, _ := time.ParseDuration(*timeoutf)
+	client := NewTelnetClient(net.JoinHostPort(flag.Arg(0), flag.Arg(1)), timeout, os.Stdin, os.Stdout)
+
+	defer client.Close()
+	if err := client.Connect(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		client.Receive()
+	}()
+	go func() {
+		defer wg.Done()
+		client.Send()
+	}()
+
+	wg.Wait()
 }
