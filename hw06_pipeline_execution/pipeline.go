35fee5e8fb36ee1e044a4a8f68f7270b86b1e5de
Lyskov Igor <igor.lyskov@vseinstrumenti.ru>
2022-03-12 22:12:48 +0300

    hw06_pipeline_execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,29 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	for _, stage := range stages {
+		if done != nil {
+			out = func(in In, done In) Out {
+				out := make(Bi)
+				go func() {
+					defer close(out)
+					for {
+						select {
+						case v, ok := <-in:
+							if !ok {
+								return
+							}
+							out <- v
+						case <-done:
+							return
+						}
+					}
+				}()
+				return out
+			}(out, done)
+		}
+		out = stage(out)
+	}
+	return out
 }
